5e6d03eb206baa63f80f376605e7041d37b6a1d0
rysekarlovic <rysekarlovic@gmail.com>
2023-07-25 21:26:32 -0500

    This is the starting skeleton of what I will be working on. Very basic for now. This is just something to get me started.

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace BankApp {
+    class Program { 
+        static void Main(string[] args)
+        {
+            int accNumber;
+            Console.WriteLine("What is your Account Number?");
+            accNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your Account Number is {accNumber}");
+        }
+    }
+
+}
