15c12383f1488435b28241557777e491baf6ad58
rysekarlovic <rysekarlovic@gmail.com>
2023-07-25 21:27:14 -0500

    Basic Skeletal structure made.

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,6 +10,14 @@ namespace BankApp {
             Console.WriteLine("What is your Account Number?");
             accNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Your Account Number is {accNumber}");
+
+            //there should be a BankAccount class that hosts members fName, lName, accountNum, balance, credit, & debits. Setters and getters should be setup for each
+
+            //Section asking for username and password, correct username and password should be private variables in a class
+
+            //If username and password is correct. Permits access to debits and credits
+
+            //In the future add a function for applying for credit card, based on certain questions should results in either approval or disapproval for credit card.
         }
     }
 
